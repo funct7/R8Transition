b5c1ce916c97d4dc5268d86f485dee73ab0004d2
Josh Park <funct7.io@gmail.com>
2022-12-04 22:26:43 +0900

    [lib] Update: FractionAnimationProviders - a new convenience factory method for UIViewControllers

--- R8Transition/Type/FractionAnimationProvider.swift
+++ R8Transition/Type/FractionAnimationProvider.swift
@@ -22,4 +22,24 @@ public enum FractionAnimationProviders {
     
     static public func create(_ block: @escaping (CGFloat) -> Void) -> FractionAnimationProvider { _Instance(block: block) }
     
+    /**
+     Creates a `FractionAnimationProvider` instance that calls `UIView.layoutIfNeeded()` at the end of the block.
+     
+     - Parameters:
+        - viewController: The `UIViewController` instance whose view will be animated by `block`.
+        - block: The animation block that sets the views in place according to the animation progress.
+        - progress: The value denoting at what point the animation is. `0.0` means the bottom sheet is at its initial position.
+     */
+    static public func makeStandard<VC>(
+        _ viewController: VC,
+        _ block: @escaping (_ viewController: VC, _ progress: CGFloat) -> Void) -> FractionAnimationProvider
+    where VC : UIViewController
+    {
+        _Instance { [weak viewController] progress in
+            guard let viewController = viewController else { return }
+            block(viewController, progress)
+            viewController.view.layoutIfNeeded()
+        }
+    }
+    
 }
